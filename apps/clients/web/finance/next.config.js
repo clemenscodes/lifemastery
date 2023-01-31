f363d2ecd987287f28554da9b307d3899bd8a52d
Clemens Horn <clemens.horn@mni.thm.de>
2023-01-31 16:42:18 +0100

    perf(shared,web-finance,web-landing): reduce bundle size
    set webpack side effects to false for barrel files

--- apps/clients/web/finance/next.config.js
+++ apps/clients/web/finance/next.config.js
@@ -2,12 +2,7 @@ const { join } = require('path');
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 const { withExpo } = require('@expo/next-adapter');
 const withPlugins = require('next-compose-plugins');
-const withTM = require('next-transpile-modules')([
-    'solito',
-    'react-native-web',
-    'nativewind',
-    'moti',
-]);
+const withTM = require('next-transpile-modules')(['solito', 'nativewind', 'moti']);
 
 module.exports = async (phase, { defaultConfig }) => {
     /**
@@ -33,6 +28,15 @@ module.exports = async (phase, { defaultConfig }) => {
             // Set this to true if you would like to to use SVGR
             // See: https://github.com/gregberge/svgr
             svgr: true,
+        },
+        // reduces bundle size and tree shaking issues a little bit
+        // see https://github.com/vercel/next.js/issues/12557#issuecomment-1351639046
+        webpack(config) {
+            config.module.rules.push({
+                test: /index\.(js|mjs|jsx|ts|tsx)$/,
+                sideEffects: false,
+            });
+            return config;
         },
     };
 
