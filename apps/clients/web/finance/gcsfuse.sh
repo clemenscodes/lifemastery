1ba1af75bc2ae2a4555cfd48d89cb40db8215f55
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-04 04:16:42 +0100

    feat(web-finance): iSR support on Google Cloud Run
    Implemented ISR by mounting Google Cloud Storage in the container and then syncing the incrementally
    generated changes to storage back and forth

--- apps/clients/web/finance/gcsfuse.sh
+++ apps/clients/web/finance/gcsfuse.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-MNT_DIR="/app/sync/dist/$APP_DIR/.next/server/pages"
-# Create mount directory for service
+SERVICE_ACCOUNT="lifemastery@landing-production-375914.iam.gserviceaccount.com"
+BUCKET_ADDRESS="gs://$BUCKET"
+CONTAINER_PAGES="$APP_HOME/dist/$APP_DIR/.next/server/pages"
+MNT_DIR="/app/gcsfuse"
+
+cmd() {
+    su - nextjs -c "$1"
+}
+
+cmd "gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$GOOGLE_APPLICATION_CREDENTIALS"
+
 mkdir -p "$MNT_DIR"
-chown nextjs:nodejs "$MNT_DIR" "$GOOGLE_APPLICATION_CREDENTIALS"
+chown -R nextjs:nodejs "$APP_HOME"
 
 echo "Mounting GCS Fuse."
 gcsfuse_command="gcsfuse -o nonempty --key-file=$GOOGLE_APPLICATION_CREDENTIALS --foreground --debug_gcs --implicit-dirs $BUCKET $MNT_DIR"
@@ -12,13 +21,23 @@ echo "$gcsfuse_command"
 exec su - nextjs -c "$gcsfuse_command" &
 echo "Mounting completed."
 
-# mv "$APP_HOME"/static "$APP_HOME"/dist/"$APP_DIR"/.next/static
+echo "Syncing newer files from bucket to app..."
+cmd "gsutil -m rsync -u -r $BUCKET_ADDRESS $CONTAINER_PAGES"
+echo "Done syncing newer files from bucket to app"
 
-# Run the web app on container startup.
-# to be equal to the cores available.
-# Timeout is set to 0 to disable the timeouts of the workers to allow Cloud Run to handle instance scaling.
-sleep 1
-exec su - nextjs -c "node /app/$APP_DIR/server.js" &
+echo "Starting Next.js app..."
+exec su - nextjs -c "node $APP_HOME/$APP_DIR/server.js" &
+echo "Next.js app started"
+
+while true; do
+    echo "Syncing changes from instance to storage..."
+    cmd "gsutil -m rsync -r $CONTAINER_PAGES $BUCKET_ADDRESS"
+    echo "Syncing changes from storage to instance..."
+    cmd "gsutil -m rsync -r $BUCKET_ADDRESS $CONTAINER_PAGES "
+    sleep 1
+done &
 
 # Exit immediately when one of the background processes terminate.
 wait -n
+
+cmd "gsutil -m rsync -r $CONTAINER_PAGES $BUCKET_ADDRESS"
