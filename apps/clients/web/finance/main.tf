002060fd533d9c6442191cabded288186e7a24b8
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-08 14:46:48 +0100

    chore(web-finance): update cloud run terraform declaration

--- apps/clients/web/finance/main.tf
+++ apps/clients/web/finance/main.tf
@@ -1,33 +1,38 @@
-provider "google" {
-  project = "finance-production-375914"
+resource "google_project" "finance-development" {
+  name       = "finance-development"
+  project_id = "finance-development-375914"
+  org_id     = "38836120782"
+  folder_id  = google_folder.finance.name
 }
 
-resource "google_cloud_run_service" "default" {
-  name                       = "finance"
-  location                   = "europe-west1"
-  autogenerate_revision_name = true
+resource "google_folder" "finance" {
+  display_name = "finance"
+  parent       = "organizations/38836120782"
+}
 
-  metadata {
-    annotations = {
-      "autoscaling.knative.dev/maxScale" = "1000"
-      "run.googleapis.com/client-name"   = "terraform"
-    }
-  }
+resource "google_cloud_run_v2_service" "default" {
+  name     = "finance"
+  location = "europe-west1"
 
   template {
-    spec {
-      containers {
-        image = "europe-west3-docker.pkg.dev/finance-production-375914/finance/web:${var.git_commit_sha}"
+    execution_environment            = "EXECUTION_ENVIRONMENT_GEN2"
+    max_instance_request_concurrency = 80
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1000
+    }
+    containers {
+      image = "europe-west3-docker.pkg.dev/finance-development-375914/finance/web:${var.git_commit_sha}"
+      ports {
+        container_port = 3000
       }
     }
   }
 
   traffic {
-    percent         = 100
-    latest_revision = true
+    percent  = 100
+    revision = true
   }
-
-
 }
 
 data "google_iam_policy" "noauth" {
@@ -38,22 +43,15 @@ data "google_iam_policy" "noauth" {
 }
 
 resource "google_cloud_run_service_iam_policy" "noauth" {
-  location = google_cloud_run_service.default.location
-  project  = google_cloud_run_service.default.project
-  service  = google_cloud_run_service.default.name
-
+  location    = google_cloud_run_v2_service.default.location
+  project     = google_cloud_run_v2_service.default.project
+  service     = google_cloud_run_v2_service.default.name
   policy_data = data.google_iam_policy.noauth.policy_data
 }
-# resource "google_cloud_run_service" "finance-development" {
-#   name     = "finance"
-#   location = "europe-west1"
 
-#   metadata {
-#     namespace = "finance-production-375914"
-#   }
-
-#   traffic {
-#     percent         = 100
-#     latest_revision = true
-#   }
-# }
+resource "google_artifact_registry_repository" "default" {
+  location      = google_cloud_run_v2_service.default.location
+  project       = google_cloud_run_v2_service.default.project
+  repository_id = "finance"
+  format        = "DOCKER"
+}
