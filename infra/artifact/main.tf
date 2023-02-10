782bec5a4315f357e706d321b79ab12fc3e14068
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-10 10:24:00 +0100

    chore(workspace): test

--- infra/artifact/main.tf
+++ infra/artifact/main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "default" {
+  format        = "DOCKER"
   location      = var.location
   project       = var.project
   repository_id = var.repository_id
-  format        = "DOCKER"
 }
