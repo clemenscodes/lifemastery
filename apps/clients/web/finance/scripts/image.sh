5edcdb5ed9f4b3ccd269c0654db342a6505fd72e
Clemens Horn <clemens.horn@mni.thm.de>
2023-02-15 19:56:39 +0100

    ci(workspace): test

---
+++ apps/clients/web/finance/scripts/image.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "No configuration (development or production) was given" && exit 1
+fi
+
+image() {
+    CONFIG="$1"
+    if [ -z "$CI" ]; then
+        INPUT_TAGS=$(git rev-parse --short HEAD) nx image web-finance --no-dte --configuration="$CONFIG"
+    else
+        if [ -z "$INPUT_GITHUB_TOKEN" ]; then
+            echo "Missing GitHub token"
+            exit 1
+        fi
+        INPUT_GITHUB_TOKEN=$INPUT_GITHUB_TOKEN INPUT_TAGS=$(git rev-parse --short HEAD) nx image web-finance --configuration="$CONFIG"
+    fi
+}
+
+case "$1" in
+development) image "$1" ;;
+production) image "$1" ;;
+*) echo "Invalid configuration: $1" && exit 1 ;;
+esac
